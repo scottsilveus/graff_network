7d8b555ac974b67aeb14cadb966acbfef8e7de12
Scott Silveus <scottsilveus@gmail.com>
2016-01-08 13:42:34 -0600

    Add Crews table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160108192706) do
+ActiveRecord::Schema.define(version: 20160108194003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "crews", force: :cascade do |t|
+    t.string   "email"
+    t.string   "name"
+    t.string   "summary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
