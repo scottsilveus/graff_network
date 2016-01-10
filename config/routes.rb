582dc76125a8adbce34ac99ce947779baf87d1a6
Scott Silveus <scottsilveus@gmail.com>
2016-01-09 22:36:28 -0600

    Add User show page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'static_pages#home'
+
+  resources :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
