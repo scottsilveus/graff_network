99703c5422e6fd6b08be0a9bf6a9938cb7b6c092
Scott Silveus <scottsilveus@gmail.com>
2016-01-09 18:35:06 -0600

    Alphabetize model validations

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -6,15 +6,15 @@ class Artwork < ActiveRecord::Base
   has_many :user_favorited_artwork_lookups
   has_many :users, through: :user_artwork_lookups, dependent: :destroy
 
-  #picture validation
-  validates :artist, length:{maximum: 50}
-  validates :description, length:{maximum: 1000}
-  validates :name, length:{maximum: 50}
-  #GPS validation
-  validates :address, length: {maximum: 150}
-  validates :city, length: {maximum: 100}
-  validates :country, length: {maximum: 100}
-  validates :state, length: {maximum: 100}
+  # picture validation
+  validates :artist,      length: { maximum: 50 }
+  validates :description, length: { maximum: 1000 }
+  validates :name,        length: { maximum: 50 }
+  # GPS validation
+  validates :address, length: { maximum: 150 }
+  validates :city,    length: { maximum: 100 }
+  validates :country, length: { maximum: 100 }
+  validates :state,   length: { maximum: 100 }
 
   validates_associated :crews, :crew_artwork_lookups, :favorited_by, :user_artwork_lookups,
     :user_favorited_artwork_lookups, :users
--- app/models/user_favorited_artwork_lookup.rb
+++ app/models/user_favorited_artwork_lookup.rb
@@ -3,5 +3,5 @@ class UserFavoritedArtworkLookup < ActiveRecord::Base
   belongs_to :artwork
 
   validates :artwork_id, presence: true
-  validates :user_id, presence: true
+  validates :user_id,    presence: true
 end
