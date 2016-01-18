968752e522fb8669d5dbf204c7838beede12cd88
Scott Silveus <scottsilveus@gmail.com>
2016-01-18 12:44:08 -0600

    Change sign in and log out routes to not begin with /users

--- app/assets/javascripts/components/home_top.js.jsx
+++ app/assets/javascripts/components/home_top.js.jsx
@@ -4,7 +4,7 @@ var HomeTop = React.createClass({
       <div className='HomeTop'>
         <h1>Unbuffable</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <a href='http://localhost:3000/users/sign_in' className='btn btn-default sign-in'>Sign In</a>
+        <a href='http://localhost:3000/signin' className='btn btn-default sign-in'>Sign In</a>
       </div>
     )
   }
